24c0af43c1b48386b7ff76e86a59bbbc0671cf4a
吳奕賓 <149990734+Yipin930@users.noreply.github.com>
2024-01-19 16:28:41 +0800

    Add files via upload

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,22 +1,23 @@
-import { Flex } from "antd";
-import logo from "@/assets/logo.png";
-import SearchCard from "./components/SearchCard";
-import CommodityCarousel from "./components/CommodityCarousel";
-import MerchantCarousel from "./components/MerchantCarousel";
-import "./style.scss";
+// views/home/index.tsx
+
+import './style.scss';
+import background from './Background.png';  // 引入背景图片
+import LogoTextFrame from './component/logo-text-frame';
+import SearchCard from './component/search-card'
+
 
 Component.displayName = "Home";
 export function Component() {
   return (
-    <Flex className="home" gap={40} vertical>
-      <div className="search-block">
-        <img className="search-block_logo" src={logo} alt="logo-text" />
-        <SearchCard />
+    <div>
+      <div className="background-image" style={{ backgroundImage: `url(${background})` }}>
+      <>
+      <LogoTextFrame />
+      </>
       </div>
-      <CommodityCarousel title="最新販賣" />
-      <CommodityCarousel title="最新收購" />
-      <MerchantCarousel title="外觀商專區" />
-      <MerchantCarousel title="代練專區" />
-    </Flex>
+      <>
+      <SearchCard/>
+      </>
+    </div>
   );
-}
+};
